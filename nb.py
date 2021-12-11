07b754ab2dd0bda1328c6e917c8575d39bed2fc8
Soenke J. Peters <peters__git@opcenter.de>
2021-12-11 11:32:57 +0100

    basic frame dissection working

--- nb.py
+++ nb.py
@@ -2,6 +2,9 @@
 
 import argparse, pathlib
 
+# width=16 poly=0x1021 init=0xffff refin=true refout=true xorout=0x0000 check=0x6f91 residue=0x0000 name="CRC-16/MCRF4XX"
+from crccheck.crc import Crc16Mcrf4Xx
+
 parser = argparse.ArgumentParser()
 parser.add_argument('filename', type=pathlib.Path, nargs='+') #type=argparse.FileType('r'))
 args = parser.parse_args()
@@ -25,40 +28,47 @@ for filename in args.filename:
             subframe_type = 0
             c = 0x01
             stuffed = False
+            inframe = False
+            crcinst = Crc16Mcrf4Xx()
 
             for n in range(len(chunk)):
                 # TODO: check which states may contain 0x71 and which require byte stuffing to 0x71 0x71
                 prev_c = c
                 c = chunk[n]
-                print(hex(c) + " "+state)
 
-                if (prev_c == 0x71):
-                    if (c == 0x71):
-                        stuffed != stuffed
-                    if stuffed != True:
-                        if (c == 0x00):
-                            state = 'framestart'
-                        if (c == 0x01):
-                            state = 'frameend'
-
-                if state == 'unknown':
-                    subframe_len = 0
-                    subframe_n = 0
-                    subframe_type = 0
-                    #prev_c = 0x01
-                    stuffed = False
-
-                    if c == 0x71:
+                if inframe == False:
+                    if c == 0x00 and prev_c == 0x71:
+                        inframe = True
+                        stuffed = False
                         state = 'framestart'
-                    else:
+                else:
+                    if c == 0x71:
+                        if stuffed:
+                            prev_c = 0xff
+                            stuffed = False
+                        else:
+                            stuffed = True
+                            continue
+                    elif stuffed:
+                        stuffed = False
+                        inframe = False
                         state = 'unknown'
-                elif state == 'framestart':
-                    if c == 0x00:
-                        state = 'subframelen'
-                    else:
-                        state = 'unknown'
+                        if c == 0x01:
+                            # frame end
+                            state = 'frameend'
+                        if c == 0x00:
+                            # frame start, should not happen here
+                            pass
+#                print(hex(c) + " "+str(inframe)+str(stuffed))
+
+                oldstate = state
+                if state == 'framestart':
+                    state = 'subframelen'
                 elif state == 'subframelen':
                     subframe_len = int(c)
+                    crcinst.process([c])
+                    subframe_n = 0
+                    subframe_type = 0
                     if subframe_len < 2:
                         state = 'unknown'
                     else:
@@ -66,37 +76,51 @@ for filename in args.filename:
                         state = 'subframetype'
                 elif state == 'subframetype':
                     subframe_type = int(c)
+                    crcinst.process([c])
                     subframe_n += 1
                     state = 'subframetstamp'
                 elif state == 'subframetstamp':
+                    crcinst.process([c])
                     subframe_n += 1
                     state = 'subframetstamp2'
                 elif state == 'subframetstamp2':
+                    crcinst.process([c])
                     subframe_n += 1
                     state = 'subframedata'
                 elif state == 'subframedata':
+                    crcinst.process([c])
                     if (subframe_n +3) < subframe_len:
                         subframe_n += 1
                     else:
                         state = 'subframecrc'
                 elif state == 'subframecrc':
+                    crcinst.process([c])
+                    subframe_n += 1
                     state = 'subframecrc2'
                 elif state == 'subframecrc2':
-                    state = 'frameendlead'
+                    crcinst.process([c])
+                    if crcinst.final() != 0:
+                        print("CRC error")
+                        exit(1)
+                    else:
+                        crcinst.reset()
+                    subframe_n += 1
+                    state = 'subframelen'
                 elif state == 'frameendlead':
                     if c == 0x71:
                         state = 'frameend'
                     else:
                         state = 'unknown'
                 elif state == 'frameend':
-                    if c == 0x00:
+                    if c == 0x01:
                         # frame complete
                         pass
                     state = 'unknown'
+                    inframe = False
                 else:
                     state = 'unknown'
 
-
+                print(hex(subframe_n) + " "+ hex(c) + " "+oldstate)
 
 
 
